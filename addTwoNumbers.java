1e136301502d17af70710731934b91e64aa8f83e
Mohamed Bengezi <bengezim@mcmaster.ca>
2020-05-16 14:24:43 -0400

    Modifying solution slightly

--- addTwoNumbers.java
+++ addTwoNumbers.java
@@ -1,30 +1,31 @@
 public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    int carry = 0;
-    ListNode p = l1;
-    ListNode q = l2;
-    
-    ListNode head = new ListNode(0);
-    ListNode result = new ListNode(0);
-    int index = 0;
-    
-    while(p != null || q != null){
-        int x = p.val;
-        int y = q.val;
-        int sum = carry + x + y;
-        carry = sum / 10;
-        result.next = new ListNode(sum%10);
-        result = result.next;
-        if (index == 0){
-            head = result;
-        }
+        int carry = 0;
+        ListNode p = l1;
+        ListNode q = l2;
         
-        index++;
-         if (p != null) p = p.next;
-         if (q != null) q = q.next;            
-
-   }
-    if (carry > 0)
-        result.next = new ListNode(carry);
-    
-    return head;
+        ListNode head = new ListNode(0);
+        ListNode result = new ListNode(0);
+        int index = 0;
+        
+		while(p != null || q != null){
+            int x = ((p == null) ? 0 : p.val);
+            int y = ((q == null) ? 0 : q.val);
+            int sum = carry + x + y;
+            carry = sum / 10;
+			sum = sum%10;
+			result.next = new ListNode(sum);
+			result = result.next;
+			if (index == 0) {
+				index = 1;
+				head = result;
+			}
+			if (p != null) p = p.next;
+			if (q != null) q = q.next;
+			
+		}
+		
+		if (carry > 0) result.next = new ListNode(carry);
+		
+		return head;
 }
+ 
