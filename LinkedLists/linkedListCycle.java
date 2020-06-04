b9bfe4759f1e177b4c3e723af34decf7b9fdee7c
Mohamed Bengezi <bengezim@mcmaster.ca>
2020-06-03 22:51:11 -0400

    Detect cycle in a linked list

---
+++ LinkedLists/linkedListCycle.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if (slow == fast) return true;
+        }
+        
+        return false;
+        
+    }
+}
