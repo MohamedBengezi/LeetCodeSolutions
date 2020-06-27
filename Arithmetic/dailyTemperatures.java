4eb24f8967373984b56978b70135c912a62f74b2
Mohamed Bengezi <bengezim@mcmaster.ca>
2020-06-27 10:44:57 -0400

    comments

--- Arithmetic/dailyTemperatures.java
+++ Arithmetic/dailyTemperatures.java
@@ -1,16 +1,19 @@
 class Solution {
     public int[] dailyTemperatures(int[] T) {
+		// Need one slow pointer and one fast
         int slow, fast, n;
         n = T.length;
         slow = fast = 0;
         int[] ans = new int[n];
         
+		// for each day in the week, increment `fast` until a hotter day appears or we reach the end of the array. 
         for (int i = 0; i < n; i++){
             int curDay = T[slow];
             int nextWarmest = 0;
             fast++;
             while (fast < n) {
                 if (T[fast] > curDay) {
+					//if we find a hotter day, nextWarmest is how many days between it and curDay
                     nextWarmest = fast - slow;
                     break;
                 } 
